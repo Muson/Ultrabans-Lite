88797a98732a73223ee37c4ed5d15476e14f35b4
deathmarine <josh@mcjoshmod.com>
2012-08-06 16:28:29 -0400

    Updated to 1.3.1

--- src/com/modcrafting/ultrabans/commands/Import.java
+++ src/com/modcrafting/ultrabans/commands/Import.java
@@ -10,9 +10,9 @@ package com.modcrafting.ultrabans.commands;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -22,8 +22,6 @@ import org.bukkit.entity.Player;
 import com.modcrafting.ultrabans.UltraBan;
 
 public class Import implements CommandExecutor{
-
-	public static final Logger log = Logger.getLogger("Minecraft");
 	boolean server = false;
 	UltraBan plugin;
 	String permission = "ultraban.import";
@@ -51,46 +49,49 @@ public class Import implements CommandExecutor{
 			@Override
 			public void run() {
 			if(!server){
-			sender.sendMessage(ChatColor.GRAY + "[UltraBan] Be patient. Loading");
-			sender.sendMessage(ChatColor.GRAY + "[UltraBan] Depending on size of list may lag the server for a moment.");
+			sender.sendMessage(ChatColor.GRAY + "Be patient. Loading");
+			sender.sendMessage(ChatColor.GRAY + "Depending on size of list may lag the server for a moment.");
 			}
-			plugin.log.log(Level.INFO, "[UltraBan] Be patient. Loading.");
-			plugin.log.log(Level.INFO, "[UltraBan] Depending on size of list may lag the server for a moment.");
+			plugin.getLogger().log(Level.INFO, "Be patient. Loading.");
+			plugin.getLogger().log(Level.INFO, "Depending on size of list may lag the server for a moment.");
 		try {
 			BufferedReader banlist = new BufferedReader(new FileReader("banned-players.txt"));
 			String p;
-			
+			//I think Mojang took my ideas and improved on them.
 			while ((p = banlist.readLine()) != null){
-					if(!plugin.bannedPlayers.contains(p.toLowerCase())){
-						plugin.db.addPlayer(p.toLowerCase(), "imported", "system", 0, 0);
-						plugin.bannedPlayers.add(p.toLowerCase());
-						
-					}
-				  }
+				if(!p.contains("#")&&p.length()>0){
+					g(p);
+				}
+			}
 			BufferedReader bannedIP = new BufferedReader(new FileReader("banned-ips.txt"));
 			String ip;
 			
 			while ((ip = bannedIP.readLine()) != null){
-					if(!plugin.bannedIPs.contains(ip))
-						plugin.bannedIPs.add(ip);
-					String cknullIP = plugin.db.getName(ip);
-					if (cknullIP != null){
-						plugin.db.addPlayer(plugin.db.getName(ip), "imported", "system", 0, 1);
-					}else{
-						plugin.db.setAddress("import", ip);
-						
-						plugin.db.addPlayer("import", "imported", "system", 0, 1);
-					}
-					Bukkit.banIP(ip);
-				  }
+				if(ip!=null&&!ip.contains("#")&&ip.length()>0){
+				    String[] args = ip.split("\\|");
+				    String name = args[0].trim();
+				    
+						if(!plugin.bannedIPs.contains(name))
+							plugin.bannedIPs.add(name);
+						String cknullIP = plugin.db.getName(name);
+						if (cknullIP != null){
+							plugin.db.addPlayer(plugin.db.getName(name), "imported", args[2].trim(), 0, 1);
+						}else{
+							plugin.db.setAddress("import", name);
+							
+							plugin.db.addPlayer("import", "imported", args[2].trim(), 0, 1);
+						}
+					  }
+					
+				}
 			
 			sender.sendMessage(ChatColor.GREEN + "Banlist imported.");
-			plugin.log.log(Level.INFO,"[UltraBan] " + "system" + " imported the banlist to the database.");
+			plugin.getLogger().log(Level.INFO,"System imported the banlist to the database.");
 			
 			return;
 			
 		} catch (IOException e) {
-			plugin.log.log(Level.SEVERE, "[Ultrabans] could not import ban list.");
+			plugin.getLogger().log(Level.SEVERE, "Could not import ban list.");
 			sender.sendMessage(ChatColor.RED + "Could not import ban list.");
 		}
 			}
@@ -99,5 +100,25 @@ public class Import implements CommandExecutor{
 		return true;
 		}
 		return false;
+	}
+
+	public void g(String line) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+	    String[] args = line.split("\\|");
+	    if(args.length<4) return;
+	    String name = args[0].trim();
+	    long temp = 0;
+	    int type = 0;
+	    try {
+			//Date conversion = format.parse(args[1].trim());
+		    if(!args[3].trim().equalsIgnoreCase("Forever")){
+		    	temp=format.parse(args[3].trim()).getTime();
+		    }
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	    String admin = args[2].trim();
+	    String reason = args[4];
+		plugin.db.addPlayer(name, reason, admin, temp, type);
 	}
 }
